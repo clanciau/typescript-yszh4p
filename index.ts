2213db11935f943224268d9ce40b2d54f3ec9e55
clanciau <christophe.lanciau@free.fr>
2019-05-27 17:39:19 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
 // Import stylesheets
-import './style.css';
+import './style.css'
 
 // Write TypeScript code!
-const myAppDiv: HTMLElement = document.getElementById('app');
-myAppDiv.innerHTML = "<a-scene><a-box color='red' position='0 2 -5' rotation='0 45 45' scale='2 2 2'> </a-box></a-scene>";
+const myAppDiv: HTMLElement = document.getElementById('app')
+
+class toto
+{
+private _value:number=10
+  get value(): number {
+        return this._value;
+    }
+
+    set value(newValue: number) {
+       console.log("setValue");
+            this._value = newValue;
+       
+    }
+}
+
+var titi=new toto
+myAppDiv.innerHTML = titi.value.toString()
+titi.value=11
+myAppDiv.innerHTML+= "<BR>"
+myAppDiv.innerHTML+= titi.value.toString()
+
+myAppDiv.innerHTML = "<a-scene><a-box color='red' position='0 2 -5' rotation='0 45 45' scale='2 2 2'> </a-box></a-scene>"
